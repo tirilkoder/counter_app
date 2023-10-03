65ee6dc10c47ca197b74ebd04dad78c5843750de
tirilkoder <tiril.koder@gmail.com>
2023-10-03 12:45:22 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Hente HTML element
+
+const plusButton = document.querySelector("#plusButton");
+console.log(plusButton);
+
+const minusButton = document.querySelector("#minusButton");
+console.log(minusButton);
+
+
+// Vi må sette opp variabler som rekner
+let count = 0;
+
+// Lage funksjoner til knappene
+function increment() {
+    count = count + 1;
+    console.log(count);
+}
+increment();
+increment();
+increment();
+increment();
+increment();
+
+function decrement() {
+    count = count - 1;
+    console.log(count);
+}
+decrement();
+decrement();
+increment();
+decrement();
+decrement();
+
